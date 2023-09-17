02e8147809a37140ad65becf576867e07d6d2579
hypernagox <128601833+hypernagox@users.noreply.github.com>
2023-09-17 11:23:34 +0900

    ipv6

--- CppStudyClient.h
+++ CppStudyClient.h
@@ -4,27 +4,109 @@
 #include <fstream>
 #include <string>
 #include <winsock2.h>
+#include <ws2tcpip.h>
 #include <string_view>
+#include <sstream>
 #pragma comment(lib, "ws2_32.lib")
 
+bool isIPv4(std::string_view address)
+{
+    std::istringstream iss(static_cast<std::string>(address));
+    std::string segment;
+    int segmentsCount = 0;
 
-bool Send_Code(std::string_view address_,std::wstring_view strSourceCode_,std::wstring_view strName_)
+    while (std::getline(iss, segment, '.'))
+    {
+        ++segmentsCount;
+
+        if (segmentsCount > 4) 
+        {
+            return false;
+        }
+
+        try
+        {
+            int value = std::stoi(segment);
+
+            if (value < 0 || value > 255)
+            {
+                return false;
+            }
+
+            if (segment.length() > 1 && segment[0] == '0')
+            {
+                return false;
+            }
+        }
+        catch (const std::exception&)
+        {
+            return false;
+        }
+    }
+
+    return segmentsCount == 4 && iss.eof();
+}
+
+std::string convertIPv4toIPv6(std::string_view ipv4)
+{
+    return std::string{ "::ffff:" } + ipv4.data();
+}
+
+std::string getLocalIP()
+{
+    char hostname[1024];
+    gethostname(hostname, sizeof(hostname));
+
+    ADDRINFO hints, * result;
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;  
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    int iResult = getaddrinfo(hostname, NULL, &hints, &result);
+    if (iResult != 0)
+    {
+        return "Error!";
+    }
+
+    char ipstringbuffer[46]; 
+    DWORD ipbufferlength = sizeof(ipstringbuffer);
+    for (ADDRINFO* ptr = result; ptr != NULL; ptr = ptr->ai_next)
+    {
+        if (ptr->ai_family == AF_INET)
+        {
+            sockaddr_in* sockaddr_ipv4 = (sockaddr_in*)ptr->ai_addr;
+            InetNtopA(AF_INET, &sockaddr_ipv4->sin_addr, ipstringbuffer, ipbufferlength);
+            freeaddrinfo(result);
+            return ipstringbuffer;
+        }
+        else if (ptr->ai_family == AF_INET6)
+        {
+            sockaddr_in6* sockaddr_ipv6 = (sockaddr_in6*)ptr->ai_addr;
+            InetNtopA(AF_INET6, &sockaddr_ipv6->sin6_addr, ipstringbuffer, ipbufferlength);
+            freeaddrinfo(result);
+            return ipstringbuffer;
+        }
+    }
+
+    freeaddrinfo(result);
+    return "Error!";
+}
+
+bool Send_Code(std::wstring_view strSourceCode_,std::wstring_view strName_,std::string address_ = "")
 {
     WSADATA wsaData;
     SOCKET s;
-    sockaddr_in server;
-    const std::string server_ip = address_.data();
-    const int server_port = 65432;             
+    sockaddr_in6 server; 
+    const int server_port = 65432;
 
-    
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
     {
         std::cerr << "Failed to initialize socket. Error: " << WSAGetLastError() << std::endl;
         return false;
     }
 
-   
-    s = socket(AF_INET, SOCK_STREAM, 0);
+    s = socket(AF_INET6, SOCK_STREAM, 0); 
 
     if (s == INVALID_SOCKET)
     {
@@ -32,12 +114,18 @@ bool Send_Code(std::string_view address_,std::wstring_view strSourceCode_,std::w
         WSACleanup();
         return false;
     }
+    
+    if (address_.empty())
+    {
+        address_ = getLocalIP();
+    }
 
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = inet_addr(server_ip.c_str());
-    server.sin_port = htons(server_port);
+    const std::string server_ip = isIPv4(address_) ? convertIPv4toIPv6(address_) : address_.data();
+    ZeroMemory(&server, sizeof(server));
+    server.sin6_family = AF_INET6;
+    inet_pton(AF_INET6, server_ip.data(), &(server.sin6_addr));
+    server.sin6_port = htons(server_port);
 
-   
     if (connect(s, (sockaddr*)&server, sizeof(server)) < 0)
     {
         std::cerr << "Connection error." << std::endl;
@@ -56,9 +144,16 @@ bool Send_Code(std::string_view address_,std::wstring_view strSourceCode_,std::w
         return false;
     }
 
-    std::wstring fileContent{ (std::istreambuf_iterator<wchar_t>(in)), std::istreambuf_iterator<wchar_t>() };
-    fileContent += L"\n//" + std::wstring{ strName_ };
+    if (send(s, reinterpret_cast<const char*>(strName_.data()), (int)(strName_.size() * sizeof(wchar_t)), 0) < 0)
+    {
+        std::cerr << "Send failed." << std::endl;
+        closesocket(s);
+        WSACleanup();
+        return false;
+    }
 
+    const std::wstring fileContent{ (std::istreambuf_iterator<wchar_t>(in)), std::istreambuf_iterator<wchar_t>() };
+   
     if (send(s, reinterpret_cast<const char*>(fileContent.data()), (int)(fileContent.size() * sizeof(wchar_t)), 0) < 0)
     {
         std::cerr << "Send failed." << std::endl;
--- 소스1.cpp
+++ 소스1.cpp
@@ -11,13 +11,13 @@ using namespace std;
 
 int main()
 {
-	String str = "helLo";
+	String str1 = "helLo";
 	String str2 = "world";
 	
-	cout<<*std::find_if(str.begin(), str.end(), isupper);
-	cout<<std::all_of(str.begin(), str.end(), islower);
+	cout<<*std::find_if(str1.begin(), str1.end(), isupper);
+	cout<<std::all_of(str2.begin(), str2.end(), islower);
 	
 	for(int i=0;i<10;++i)
-		Send_Code("192.168.0.43", L"¼Ò½º1.cpp",L"ÃÖÀçÇõ");
+		Send_Code(L"¼Ò½º1.cpp",L"ÃÖÀçÇõ");
 	
 }
